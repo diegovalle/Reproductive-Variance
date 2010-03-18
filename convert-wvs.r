ca6b36a4912ce7bf16273bf893f8c5b35e977055
Diego Valle Jones <diegovalle@gmail.com>
2010-03-17 20:00:23 -0600

    uft8 without BOM

--- convert-wvs.r
+++ convert-wvs.r
@@ -1,4 +1,4 @@
-﻿#Only run this if you downloaded the original wvs 2000 and 2005
+#Only run this if you downloaded the original wvs 2000 and 2005
 #stata data files! http://www.wvsevsdb.com/wvs/WVSData.jsp?Idioma=I
 library(foreign)
 
@@ -11,7 +11,7 @@ library(foreign)
 wvs <- read.dta("data/wvs2000_v20090914.dta")
 #"year","country","childs","sex","age"
 write.csv(wvs[ ,c("v1", "v2", "v107", "v223", "v225")],
-          "num-child2000.csv")
+          "data/num-child2000.csv")
 rm(wvs)
 
 #################################################################
@@ -22,13 +22,13 @@ rm(wvs)
 #WVS2005-List A official file v.20090901 compressed in ZIP format (SPSS=8.6Mb, SAS=14.6Mb, STATA=7.6Mb)
 wvs <- read.dta("data/wvs2005_v20090901a.dta")
 write.csv(wvs[ ,c("v1", "v2", "v56", "v235", "v237")],
-          "num-child2005.csv")
+          "data/num-child2005.csv")
 rm(wvs)
 
 #WVS2005-List B official file v.20090901 compressed in ZIP format (SPSS=1.8Mb, SAS=2.8Mb, STATA=1.5Mb)
 wvs <- read.dta("data/wvs2005_v20090901b.dta")
 write.csv(wvs[ ,c("v1", "v2", "v56", "v235", "v237")],
-          "num-child2005b.csv")
+          "data/num-child2005b.csv")
 rm(wvs)
 
 ########################################################
@@ -39,5 +39,5 @@ rm(wvs)
 #WVS official 5 wave aggregate 1981-2008 v.20090914 compressed in ZIP format (SPSS=45Mb, SAS=87Mb, STATA=39Mb) 	SPSS  SAS  STATA
 #wvs <- read.dta("data/wvs1981_2008_v20090914.dta")
 #write.csv(wvs[ ,c("v1", "v2", "v56", "v235", "v237")],
-#          "num-child1981_2008.csv")
+#          "data/num-child1981_2008.csv")
 #rm(wvs)
